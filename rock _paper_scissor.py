6fbbaa1bcefc792f339cd7cd2e2490527cd113a4
VaishSabbanwar <168840863+VaishSabbanwar@users.noreply.github.com>
2024-07-19 00:20:11 +0530

    Update rock _paper_scissor.py

--- rock _paper_scissor.py
+++ rock _paper_scissor.py
@@ -1,52 +1,67 @@
-import random
+import random 
 
-l=["rock","paper","scissor"]
-
-'''
-rock vs paper >>paper wins
-rock vs scissor >>rock wins
-paper vs scissor >>scissor wins
-'''
+# List of choices for the game
+choices = ["rock", "paper", "scissor"]
 
 while True:
-    ccount=0
-    ucount=0
-    uc=int(input('''
+    ccount = 0  # Counter for computer wins
+    ucount = 0  # Counter for user wins
+    
+    # Prompting the user to start the game or exit
+    uc = int(input('''
      Game Start...
-     1.Yes
-     2.No|Exit       
+     1. Yes
+     2. No | Exit       
             
+    '''))
+    
+    if uc == 1:
+        # Loop for 5 rounds of the game
+        for _ in range(5):
+            userInput = int(input('''
+            1. Rock 
+            2. Paper
+            3. Scissor            
             '''))
-    if uc==1:
-        for a in range(1,6):
-            userInput= int(input('''
-       1.Rock 
-       2.Paper
-       3.Scissor            
-                     '''))
-            if userInput==1:
-                userchoice= "rock"
-            elif userInput==2:
-                userchoice= "paper"
-            elif userInput==3:
-                userchoice= "scissor"  
-            compChoice=random.choice(l)
-            print("COMPUTER VALUE",compChoice)
-            print("USER VALUE",userchoice)
-            if compChoice==userchoice:
-                print("GAME DRAW")   
-                ucount= ucount+1
-                ccount= ccount+1
-            elif (userchoice=="rock" and compChoice=="scissor") or (userchoice=="paper" and compChoice=="rock") or (userchoice=="scissor" and compChoice=="paper"):
-                print("YOU WIN")
-                ucount=ucount+1
+            
+            # Mapping user input to choices
+            if userInput == 1:
+                userchoice = "rock"
+            elif userInput == 2:
+                userchoice = "paper"
+            elif userInput == 3:
+                userchoice = "scissor"
             else:
-                print("COMPUTER WIN")
-                ccount= ccount+1
-        print("FINAL SCORE - ")
-        print("COMPUTER : ", ccount)
-        print("USER : ", ucount)
-    elif uc==2:
+                print("Invalid input. Please enter a number from 1 to 3.")
+                continue
+                
+            # Randomly selecting computer's choice
+            compChoice = random.choice(choices)
+            print("COMPUTER VALUE:", compChoice)
+            print("USER VALUE:", userchoice)
+            
+            # Determining the result of the round
+            if compChoice == userchoice:
+                print("GAME DRAW")
+                ucount += 1
+                ccount += 1
+            elif (userchoice == "rock" and compChoice == "scissor") or \
+                 (userchoice == "paper" and compChoice == "rock") or \
+                 (userchoice == "scissor" and compChoice == "paper"):
+                print("YOU WIN")
+                ucount += 1
+            else:
+                print("COMPUTER WINS")
+                ccount += 1
+        
+        # Printing final scores after 5 rounds
+        print("\nFINAL SCORE - ")
+        print("COMPUTER:", ccount)
+        print("USER:", ucount)
+        
+    elif uc == 2:
+        # Exiting the game if user chooses to exit
         break
     else:
+        # Handling invalid input
         print("Invalid input. Please enter 1 or 2.")
